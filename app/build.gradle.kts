f0e601a67d78fba01aa3ed28ab4a4c74c87680aa
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-19 17:32:25 +0600

    Add Google Credential Manager dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,9 @@ dependencies {
 
     implementation("com.google.firebase:firebase-auth:23.2.1")
     implementation("com.google.firebase:firebase-firestore:25.1.4")
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
