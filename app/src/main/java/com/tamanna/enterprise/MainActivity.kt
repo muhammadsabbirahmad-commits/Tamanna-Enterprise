52b3008ebc6bf598aef94ec0e7d158e21613ead0
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-19 12:12:43 +0600

    Connect app launch to optional login

--- app/src/main/java/com/tamanna/enterprise/MainActivity.kt
+++ app/src/main/java/com/tamanna/enterprise/MainActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.tamanna.enterprise.dashboard.DashboardActivity
+import com.tamanna.enterprise.security.LoginActivity
+import com.tamanna.enterprise.security.SecurityStorage
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(
-            Intent(this, DashboardActivity::class.java)
-        )
+        SecurityStorage.ensureInitialized(this)
+        val destination = if (SecurityStorage.isLoginEnabled(this) && !SecurityStorage.isLoggedIn(this)) {
+            LoginActivity::class.java
+        } else {
+            DashboardActivity::class.java
+        }
+
+        startActivity(Intent(this, destination))
 
         finish()
     }
