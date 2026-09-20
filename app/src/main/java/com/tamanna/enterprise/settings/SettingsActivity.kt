92948441ae797dce6dd15481a654caa461c9eae8
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-20 11:26:58 +0600

    Fix Settings login Intent import

--- app/src/main/java/com/tamanna/enterprise/settings/SettingsActivity.kt
+++ app/src/main/java/com/tamanna/enterprise/settings/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.tamanna.enterprise.settings
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.Manifest
 import android.os.Build
