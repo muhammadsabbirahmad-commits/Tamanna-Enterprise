295fbd985afd38e5f08d7b66d3922d2403c60de2
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-23 17:29:26 +0600

    Start Gmail approval access flow

--- app/src/main/java/com/tamanna/enterprise/MainActivity.kt
+++ app/src/main/java/com/tamanna/enterprise/MainActivity.kt
@@ -3,23 +3,14 @@ package com.tamanna.enterprise
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.tamanna.enterprise.business.BusinessAccessActivity
-import com.tamanna.enterprise.business.LicenseActivationActivity
-import com.tamanna.enterprise.business.LicenseStorage
-import com.tamanna.enterprise.notifications.NotificationScheduler
+import com.tamanna.enterprise.security.LoginActivity
 import com.tamanna.enterprise.security.SecurityStorage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SecurityStorage.ensureInitialized(this)
-        NotificationScheduler.scheduleDaily(this)
-
-        if (!LicenseStorage.isActive(this)) {
-            startActivity(Intent(this, LicenseActivationActivity::class.java))
-        } else {
-            startActivity(Intent(this, BusinessAccessActivity::class.java))
-        }
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
