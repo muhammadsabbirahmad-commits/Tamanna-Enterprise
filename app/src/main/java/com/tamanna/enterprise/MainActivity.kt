cc2fcc4b56088fce2d81ab40c45ef1ad0715cf82
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-24 13:53:42 +0600

    Update MainActivity.kt

--- app/src/main/java/com/tamanna/enterprise/MainActivity.kt
+++ app/src/main/java/com/tamanna/enterprise/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tamanna.enterprise
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.tamanna.enterprise.dashboard.DashboardActivity
 import com.tamanna.enterprise.security.LoginActivity
 import com.tamanna.enterprise.security.SecurityStorage
 
@@ -10,7 +11,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SecurityStorage.ensureInitialized(this)
-        startActivity(Intent(this, LoginActivity::class.java))
+        
+        // যদি লগইন করা থাকে তবে সরাসরি ড্যাশবোর্ডে যাবে, নাহলে লগইন স্ক্রিনে
+        if (SecurityStorage.isLoggedIn(this)) {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
         finish()
     }
 }
